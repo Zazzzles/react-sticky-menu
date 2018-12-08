536607a18ec88fb152191b3c332b1c4dbc157c3c
Ras Brits <ras@fullfacing.com>
2018-12-08 20:33:32 +0200

    Added spacer

--- src/components/StickyMenu.js
+++ src/components/StickyMenu.js
@@ -1,44 +1,54 @@
 import React, { Component } from 'react';
 
 import '../styles/sticky.css'
-
 class StickyMenu extends Component {
 
-   constructor(props){
-       super(props)
-       this.state = {
-        headerSticky: false
-       }
-       this.topOffset = 0
-   }
-
-    componentDidMount () {
-        this.topOffset = this.refs.wrapper.offsetTop
-        window.addEventListener('scroll', () =>{
-          if(this.topOffset < window.pageYOffset){
-            this.setState({
-                headerSticky: true
-            })
-          }else{
-            this.setState({
-                headerSticky: false
-            })
-          }
-        })
+    constructor(props){
+        super(props)
+        this.state = {
+         headerSticky: false
+        }
+        this.topOffset = 0
+        this.headerHeight = 0
     }
-
-    shouldComponentUpdate (next) {
-        return next.headerSticky !== this.state.headerSticky
-    }
-
-    render() {
-        const { headerSticky } = this.state 
-        return(
-            <div className={`sticky-wrapper ${headerSticky ? 'sticky' : ''}`} ref='wrapper'>
-                {this.props.children}
-            </div>
-        );
-    }
-}
-
-export default StickyMenu;
+ 
+     componentDidMount = () => {
+         this.topOffset = this.refs.wrapper.offsetTop
+         this.headerHeight = this.refs.wrapper.clientHeight
+         console.log(this.headerHeight)
+         window.addEventListener('scroll', () =>{
+           if(this.topOffset < window.pageYOffset){
+             this.setState({
+                 headerSticky: true
+             })
+           }else{
+             this.setState({
+                 headerSticky: false
+             })
+           }
+         })
+     }
+ 
+     shouldComponentUpdate = (next) => next.headerSticky !== this.state.headerSticky
+ 
+     render() {
+         const { headerSticky } = this.state 
+         return(
+             <div>
+                 <div className={`sticky-wrapper ${headerSticky ? 'sticky' : ''}`} ref='wrapper'>
+                     {this.props.children}
+                 </div>
+                 {
+                     headerSticky ?
+                     <div style={{height: this.headerHeight}}>
+ 
+                     </div> : null
+                 }
+               
+             </div>
+            
+         );
+     }
+ }
+ 
+ export default StickyMenu;
